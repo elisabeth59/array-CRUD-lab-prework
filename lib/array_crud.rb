5f98eee42da1e72b589fdac4d98f18a59480da5d
Elisabeth Narine <elisabethnarine@gmail.com>
2018-05-06 15:35:49 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  fav_fruits = ["apples", "oranges", "bananas", "peaches"]
+  fav_fruits.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
