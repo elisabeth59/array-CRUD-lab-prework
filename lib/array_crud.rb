dcfcd0ccffe139518f2d3273bc1eb4acaa5be963
Elisabeth Narine <elisabethnarine@gmail.com>
2018-05-06 15:30:42 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   fav_fruits = ["apples", "oranges", "bananas", "peaches"]
-  fav_fruits.push("strawberries")
+  fav_fruits.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
