6c824f92036c1b8d1080e6680bd866174227a3ff
Elisabeth Narine <elisabethnarine@gmail.com>
2018-05-06 16:44:59 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fav_fruits = ["wow", "I", "am", "really", "learning"]
+  fav_fruits = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
